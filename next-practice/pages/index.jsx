edca6cd6fe89c6d628af17a85fc38448ef6133e5
minjuice1 <minjuice93@gmail.com>
2022-03-21 11:56:31 +0900

    feat: ssr로 api call 하여, index 페이지에 해당 api post의 title 추가

--- next-practice/pages/index.jsx
+++ next-practice/pages/index.jsx
@@ -1,8 +1,27 @@
 
-export default function Home() {
+export default function Home({posts}) {
+  console.log(posts);
+
   return (
     <>
       <h1>Welcome to my blog</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </>
   )
+}
+
+// 해당 페이지를 들어올때마다 데이터를 그때 그때 요청하여 렌더링해줌.
+export const getServerSideProps = async() => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_start=0&_end=10')
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
