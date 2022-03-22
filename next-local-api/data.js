c39c139f975c1fb9bf6cc4faff49114c03ef33f9
minjuice1 <minjuice93@gmail.com>
2022-03-22 18:26:31 +0900

    edit: server api data 복구

--- next-local-api/data.js
+++ next-local-api/data.js
@@ -3,7 +3,7 @@ module.exports = {
     {
     userId: 1,
     id: 1,
-    title: "hahahhahahahahah",
+    title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
     body: "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
     },
     {
