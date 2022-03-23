60d76dd6ba85e232ad45e62cd5fa65daf9646e14
minjuice1 <minjuice93@gmail.com>
2022-03-23 14:26:31 +0900

    feat:photo 페이지에 SSG로 외부 api photos 호출

--- next-practice/pages/photos.jsx
+++ next-practice/pages/photos.jsx
@@ -1,12 +1,33 @@
 import HeadInfo from '../components/HeadInfo'
+import Image from 'next/image'
 
-const photos = () => {
+const photos = ({photos}) => {
+  console.log(photos);
+
   return (
     <>
       <HeadInfo title="My Blog Photos"/>
       <h1>My photos</h1>
+      <ul>
+        {photos.map(photo => (
+          <li key={photo.id}>
+            <Image src={photo.thumbnailUrl} width={100} height={100} alt={photo.title}/>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
 
-export default photos
+export default photos;
+
+export const getStaticProps = async() => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/photos?_start=0&_end=10');
+  const photos = await res.json();
+
+  return {
+    props: {
+      photos
+    }
+  }
+}
