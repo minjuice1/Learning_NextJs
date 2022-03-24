5ee96ae0cb817fd45faa2cfa9ebdd8832f115c9a
minjuice1 <minjuice93@gmail.com>
2022-03-24 17:26:31 +0900

    style: photo 페이지 image style 수정

--- next-practice/pages/photos.jsx
+++ next-practice/pages/photos.jsx
@@ -1,17 +1,22 @@
 import HeadInfo from '../components/HeadInfo'
 import Image from 'next/image'
+import photoStyles from "../styles/Photos.module.css"
 
 const photos = ({photos}) => {
-  console.log(photos);
-
   return (
     <>
       <HeadInfo title="My Blog Photos"/>
       <h1>My photos</h1>
-      <ul>
+      <ul className={photoStyles.photos}>
         {photos.map(photo => (
           <li key={photo.id}>
-            <Image src={photo.thumbnailUrl} width={100} height={100} alt={photo.title}/>
+            <Image 
+            src={photo.thumbnailUrl} 
+            width={100} 
+            height={100} 
+            alt={photo.title}
+            />
+            <span>{photo.title}</span>
           </li>
         ))}
       </ul>
